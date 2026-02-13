e391da0fd1f5931eb553ff4382b59f21857bb69b
lilitantonyan <lilit@helix.am>
2026-02-13 21:39:53 +0400

    add fixes

--- js/main.js
+++ js/main.js
@@ -8,5 +8,73 @@ document.addEventListener("DOMContentLoaded", (event) => {
         document.querySelector('body').classList.toggle('no-scroll');
     })
 
+    var swiper = new Swiper(".news__list", {
+        slidesPerView: 3,
+        spaceBetween: 20,
+        breakpoints: {
+            0:{
+                slidesPerView: 1,
+                spaceBetween: 20,
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                },
+            },
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                },
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                },
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 50,
+            },
+        },
+    });
+    var swiper1 = new Swiper(".tradition__list", {
+        slidesPerView: 3,
+        spaceBetween: 20,
+        breakpoints: {
+            0:{
+                slidesPerView: 1,
+                spaceBetween: 20,
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                },
+            },
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                },
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                },
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 50,
+            },
+        },
+    });
 
 });
